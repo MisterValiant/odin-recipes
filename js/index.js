c0e537ff9d4110ac1b1ae27d24eb89e98c95fa95
MisterValiant <81043907+MisterValiant@users.noreply.github.com>
2022-08-20 05:28:01 +0400

    Day-Night cycle
    Added: Day-Night cycle and click sound in header

--- js/index.js
+++ js/index.js
@@ -1,15 +1,102 @@
 var audio = new Audio('audio/genshin-bgm.mp3');
+var audioNight = new Audio('audio/genshin-watatsumi.mp3');
+var clickAudio = new Audio('audio/minecraft-click.mp3');
+var muteAudio = new Audio('audio/discord-mute.mp3');
+var currentTimeInHr = (new Date()).getHours();
+
+window.onload = function () {
+    if ((currentTimeInHr >= 6 && currentTimeInHr < 18)) {
+        document.getElementById('my-checkbox').checked = false;
+        day();
+    } else {
+        document.getElementById('my-checkbox').checked = true;
+        night();
+    }
+}
 
 function audioController() {
-    if (!audio.paused) {
-        audio.pause();
-        document.getElementById('audio-player').src='images/pause.png';
-        document.getElementById('audio-player').title='Play';
+    if (document.getElementById('my-checkbox').checked === false) {
+        if (!audio.paused) {
+            pauseAudio();
+        } else {
+            audio.play();
+            audio.loop = true;
+            audio.volume = 0.6;
+            playAudio();
+        }
     } else {
-        audio.play();
-        audio.loop=true;
-        audio.volume = 0.8;
-        document.getElementById('audio-player').src='images/play.png';
-        document.getElementById('audio-player').title='Pause';
+        if (!audioNight.paused) {
+            pauseAudio();
+        } else {
+            audioNight.play();
+            audioNight.loop = true;
+            audioNight.volume = 0.6;
+            playAudio();
+        }
     }
+}
+
+// Audio Controls
+
+function playAudio() {
+    clickAudio.volume = 0.2;
+    clickAudio.play();
+
+    var audioPlayer = document.getElementById('audio-player');
+    audioPlayer.src = 'images/play.png';
+    audioPlayer.title = 'Pause';
+}
+
+function pauseAudio() {
+    muteAudio.volume = 0.2;
+    muteAudio.play();
+
+    audio.pause();
+    audioNight.pause();
+
+    var audioPlayer = document.getElementById('audio-player');
+    audioPlayer.src = 'images/pause.png';
+    audioPlayer.title = 'Play';
+}
+
+function resetAudio() {
+    audio.pause();
+    audio.currentTime = 0;
+    audioNight.pause();
+    audioNight.currentTime = 0;
+}
+
+// Day-Night Controls
+
+function day() {
+    var mySwitch = document.getElementById('my-switch');
+    mySwitch.style.backgroundColor = '#87CEEB';
+    mySwitch.title = 'Toggle night';
+
+    var icon = document.getElementById('switch-icon');
+    icon.classList.remove("bi-stars");
+    icon.classList.add("bi-brightness-alt-high-fill");
+}
+
+function night() {
+    var mySwitch = document.getElementById('my-switch');
+    mySwitch.style.backgroundColor = '#001c7c';
+    mySwitch.title = 'Toggle day';
+
+    var icon = document.getElementById('switch-icon');
+    icon.classList.remove("bi-brightness-alt-high-fill");
+    icon.classList.add("bi-stars");
+}
+
+function modeCycle() {
+    clickAudio.volume = 0.2;
+    clickAudio.play();
+
+    if (document.getElementById('my-checkbox').checked) {
+        night();
+    } else {
+        day();
+    }
+    
+    resetAudio();
 }
