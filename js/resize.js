d35788ef28dd5aac42fe4aa5588ec8990cd878d4
MisterValiant <81043907+MisterValiant@users.noreply.github.com>
2022-09-06 01:59:43 +0400

    Responsiveness & UI
    + width & height responsiveness
    + Better responsive UI
    + Changed background for liyue and inazuma-day for better visibility

--- js/resize.js
+++ js/resize.js
@@ -1,11 +1,11 @@
-var htmlDoc = document.querySelector('html');
+// var htmlDoc = document.querySelector('html');
 
-//Max supported screen
-window.onresize = function () {
-    if (window.screen.availHeight < 740 || window.screen.availWidth < 1069) {
-        htmlDoc.style.filter='blur(30px)';
-        alert('Thank you for using Odin Recipes! 🤗 The website currently only supports a min resolution of 1069x740. MisterValiant is currently fixing the issue, stay tune!');
-    } else {
-        htmlDoc.style.filter='blur(0px)';
-    }
-}
+// //Max supported screen
+// window.onresize = function () {
+//     if (window.screen.availHeight < 740 || window.screen.availWidth < 1069) {
+//         htmlDoc.style.filter='blur(30px)';
+//         alert('Thank you for using Odin Recipes! 🤗 The website currently only supports a min resolution of 1069x740. MisterValiant is currently fixing the issue, stay tune!');
+//     } else {
+//         htmlDoc.style.filter='blur(0px)';
+//     }
+// }
